233a26b2c7f929dad2b6a19586392ca4fd6b5769
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-06-01 14:47:51 +0300

    feat():Create Logger class (gMKVLogger)

---
+++ src/extract/Logger.cpp
@@ -0,0 +1,32 @@
+#include "Logger.h"
+
+namespace qmkv::extract
+{
+    Logger::Logger(QObject *parent)
+        : QObject{parent}
+    {
+    }
+
+    constexpr QLatin1StringView emptyString("");
+
+    QString Logger::LogText() const
+    {
+        return m_log.join(emptyString);
+    }
+
+    void Logger::clear()
+    {
+        m_log.clear();
+    }
+
+    void Logger::log(QStringView message)
+    {
+        auto now = QDateTime::currentDateTime();
+        auto nowStr = now.toString("[yyyy-MM-dd][HH:mm:ss]");
+        nowStr.reserve(message.length() + 1);
+        auto logMessage = nowStr.append(" ").append(message);
+        m_log.append(logMessage);
+        emit lineAdded(QStringView(logMessage), now);
+    }
+
+}
---
+++ src/extract/include/Logger.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <QDateTime>
+#include <QObject>
+
+namespace qmkv::extract {
+    class Logger : public QObject
+    {
+        Q_OBJECT
+    public:
+        explicit Logger(QObject* parent = nullptr);
+
+        QString LogText() const;
+        static Logger* getInstance();
+    public slots:
+        void clear();
+        void log(QStringView message);
+
+    signals:
+        void lineAdded(QStringView  line, QDateTime dt);
+
+    protected:
+        QStringList m_log;
+  static Logger * m_instance;
+    };
+} // namespace qmkv::extract
