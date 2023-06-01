e1e50fc5d08544a1e404ac5a310502597e177905
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-06-01 14:54:12 +0300

    test():Add test code for test SettingsManager

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -1,6 +1,7 @@
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
 #include "Helper.h"
+#include "SettingsManager.h"
 
 MainWindow::MainWindow(QWidget* parent)
         : QMainWindow(parent)
@@ -12,6 +13,10 @@ MainWindow::MainWindow(QWidget* parent)
         QString title("0 1\"2:3#4\\5[6]");
         title += helper.EscapeString(title);
         setWindowTitle(title);
+        QSharedPointer<qmkv::extract::Logger> logger;
+        qmkv::model::SettingsManager sm{logger,this};
+        auto sett = sm.get();
+        sett->setShowPopup(true);
 }
 
 MainWindow::~MainWindow()
