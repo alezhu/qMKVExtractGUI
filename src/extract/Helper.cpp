86a98fc3e6578d801045862bcffad79a16688794
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-06-01 14:46:55 +0300

    feat():Create Helper class (start port gMKVHelper)

---
+++ src/extract/Helper.cpp
@@ -0,0 +1,47 @@
+#include "Helper.h"
+#include <QtGlobal>
+
+namespace qmkv::extract
+{
+  Helper::Helper() {}
+
+  constexpr std::array<std::pair<QLatin1StringView, QLatin1StringView>, 7>
+      escape_pairs = {{
+          {R"(\s)"_L1, " "_L1},
+          {R"(\2)"_L1, "\""_L1},
+          {R"(\c)"_L1, ":"_L1},
+          {R"(\h)"_L1, "#"_L1},
+          {R"(\\)"_L1, "\\"_L1}, //????
+          {R"(\b)"_L1, "["_L1},
+          {R"(\B)"_L1, "]"_L1},
+      }};
+
+  QString processPairs(
+      QString str,
+      std::function<QString(
+          QString, const std::pair<QLatin1StringView, QLatin1StringView> &)>
+          fn)
+  {
+    auto result = str;
+    for (auto &&pair : escape_pairs)
+    {
+      result = fn(result, pair);
+    }
+    return result;
+  }
+
+  QString Helper::UnescapeString(QString argString)
+  {
+    auto result = processPairs(argString, [](auto str, auto pair)
+                               { return str.replace(pair.first, pair.second); });
+    return result;
+  }
+
+  QString Helper::EscapeString(QString argString)
+  {
+    auto result = processPairs(argString, [](auto str, auto pair)
+                               { return str.replace(pair.second, pair.first); });
+    return result;
+  }
+
+} // namespace qmkv::extract
---
+++ src/extract/include/Helper.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <QLatin1StringView>
+
+using namespace Qt::Literals::StringLiterals;
+
+namespace qmkv::extract {
+    class Helper
+    {
+    public:
+        Helper();
+
+        inline constexpr bool IsOnLinux() const noexcept
+        {
+#ifdef Q_OS_WIN
+            return false;
+#else
+            return true;
+#endif
+        };
+
+        inline constexpr QLatin1StringView MKV_MERGE_GUI_FILENAME() const noexcept
+        {
+#ifdef Q_OS_WIN
+            return "mmg.exe"_L1;
+#else
+            return "mmg"_L1;
+#endif
+        };
+
+        inline constexpr QLatin1StringView MKV_MERGE_NEW_GUI_FILENAME() const noexcept
+        {
+#ifdef Q_OS_WIN
+            return "mkvmerge.exe"_L1;
+#else
+            return "mkvmerge"_L1;
+#endif
+        };
+
+        QString UnescapeString(QString argString);
+        QString EscapeString(QString argString);
+    };
+} // namespace qmkv::extract
