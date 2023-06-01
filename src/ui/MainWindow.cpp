14c27c157e1492176886f89e23e0baaaa739558b
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-06-01 14:50:57 +0300

    test():Add test code for Helper::EscapeString

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -1,5 +1,6 @@
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
+#include "Helper.h"
 
 MainWindow::MainWindow(QWidget* parent)
         : QMainWindow(parent)
@@ -7,6 +8,10 @@ MainWindow::MainWindow(QWidget* parent)
 {
         ui->setupUi(this);
 
+        qmkv::extract::Helper helper;
+        QString title("0 1\"2:3#4\\5[6]");
+        title += helper.EscapeString(title);
+        setWindowTitle(title);
 }
 
 MainWindow::~MainWindow()
