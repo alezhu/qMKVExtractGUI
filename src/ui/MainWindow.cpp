68b237a662ce54d6e00768d63c63b1a4458a66d8
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-06-02 10:43:56 +0300

    test():Remove test code from MainWindow constructor

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -11,14 +11,6 @@ MainWindow::MainWindow(QWidget* parent)
 {
         ui->setupUi(this);
 
-        qmkv::extract::Helper helper;
-        QString title("0 1\"2:3#4\\5[6]");
-        title += helper.EscapeString(title);
-        setWindowTitle(title);
-        QSharedPointer<qmkv::extract::Logger> logger(new qmkv::extract::Logger());
-        qmkv::model::SettingsManager sm{logger,this};
-        auto sett = sm.get();
-        sett->setShowPopup(true);
 }
 
 MainWindow::~MainWindow()
