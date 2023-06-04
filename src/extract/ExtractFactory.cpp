313b77ca560734117e11bd2f40713c9c20dc9c56
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-06-04 20:47:43 +0300

    feat():Create ExtractFactory class

---
+++ src/extract/ExtractFactory.cpp
@@ -0,0 +1,12 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com//
+// Created by alezhu on 04.06.2023.
+//
+
+#include "ExtractFactory.h"
+
+namespace qmkv::extract {
+    Extract *ExtractFactory::create(QAnyStringView path) {
+        return new Extract(path);
+    }
+}
---
+++ src/extract/include/ExtractFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by alezhu on 04.06.2023.
+//
+
+#pragma once
+
+#include "Extract.h"
+
+namespace qmkv::extract {
+    class ExtractFactory {
+    public:
+        Extract *create(QAnyStringView path);
+    };
+}
