a8fadcdda31b57374c10f0aad089af335dac2db5
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-06-04 20:52:38 +0300

    feat():Adapt model creation

--- src/ui/Application.cpp
+++ src/ui/Application.cpp
@@ -3,15 +3,18 @@
 #include <QApplication>
 #include <SettingsManager.h>
 #include "MainWindow.h"
+#include "utils.h"
 
 int Application::start(int argc, char *argv[]) {
     QCoreApplication::setOrganizationName("Alezhu");
     QCoreApplication::setApplicationName("qMKVExtractGUI");
 
     qmkv::extract::Logger logger;
+    qmkv::extract::Helper helper;
     QApplication app(argc, argv);
     qmkv::model::SettingsManager sm{logger};
-    qmkv::model::Model model{};
+    qmkv::extract::ExtractFactory extractFactory;
+    qmkv::model::Model model{&logger, &helper, &sm, &qmkv::ui::showErrorMessage, extractFactory};
 
     MainWindow w{sm, model};
     w.show();
