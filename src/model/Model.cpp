21cb812989c6f7351c697679935bcdbb9cff296d
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-06-04 20:51:21 +0300

    feat():Add check logic into MKVtoolnixPath setter

--- src/model/Model.cpp
+++ src/model/Model.cpp
@@ -1,9 +1,21 @@
+#include <QFile>
 #include "include\Model.h"
+#include "path.h"
 
 namespace qmkv::model {
+    Model::Model(
+            extract::Logger *logger,
+            extract::Helper *helper,
+            SettingsManager *settingsManager,
+            const std::function<void(QAnyStringView, QWidget *)> &showErrorMessage,
+            extract::ExtractFactory &extractFactory,
+            QObject *parent)
+            : QObject(parent),
+              m_logger{logger},
+              m_helper{helper},
+              m_settingsManager{settingsManager},
+              m_showErrorMessage{showErrorMessage} {
 
-    Model::Model(QObject *parent)
-            : QObject{parent} {
     }
 
     QStringView Model::MKVToolnixPath() const {
@@ -12,7 +24,22 @@ namespace qmkv::model {
 
     void Model::setMKVToolnixPath(const QStringView value) {
         if (m_MKVToolnixPath == value) return;
-        m_MKVToolnixPath = value.toString();
-        emit MKVToolnixPathChanged(value);
+        auto path = value.trimmed().toString();
+
+        if (!QFile(core::pathCombine(path, m_helper->MKV_MERGE_GUI_FILENAME())).exists()
+            && !QFile(core::pathCombine(path, m_helper->MKV_MERGE_NEW_GUI_FILENAME())).exists()
+                ) {
+            path.clear();
+            auto msg = tr("The folder does not contain MKVToolnix!");
+            m_logger->log(msg);
+            qDebug() << msg;
+            m_showErrorMessage(msg, nullptr);
+        } else {
+            m_settingsManager->get()->setMkvToolnixPath(path);
+            m_logger->log(tr("Changing MkvToolnixPath"));
+        }
+
+        m_MKVToolnixPath = path;
+        emit MKVToolnixPathChanged(m_MKVToolnixPath);
     }
 }
--- src/model/include/Model.h
+++ src/model/include/Model.h
@@ -2,16 +2,28 @@
 
 #include <QObject>
 #include "common.h"
+#include "SettingsManager.h"
+#include "Helper.h"
+#include "ExtractFactory.h"
 
-namespace qmkv::model
-{
-    class Model : public QObject
-    {
-        Q_OBJECT
+namespace qmkv::model {
+    class Model : public QObject {
+    Q_OBJECT
+
     public:
-        explicit Model(QObject *parent = nullptr);
+        explicit Model(
+                extract::Logger *logger,
+                extract::Helper *helper,
+                SettingsManager *settingsManager,
+                const std::function<void(QAnyStringView, QWidget *)> &showErrorMessage,
+                QObject *parent = nullptr);
 
-        Q_PROPERTY_STD_EX(QString, QStringView, MKVToolnixPath);
+    Q_PROPERTY_STD_EX(QString, QStringView, MKVToolnixPath);
 
+    private:
+        extract::Logger *m_logger;
+        extract::Helper *m_helper;
+        SettingsManager *m_settingsManager;
+        const std::function<void(QAnyStringView, QWidget *)> &m_showErrorMessage;
     };
 }
